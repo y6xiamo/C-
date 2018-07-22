29a5ad46477906f0cbdbc57e1654fe11a97f6796
y6xiaomo <xiamo6512@163.com>
2018-07-22 17:37:32 +0800

    test.c

--- test.c
+++ test.c
@@ -1,13 +1,17 @@
 #include<iostream>
-#include <stdio.h>
-#include<string.h>
-
+#include<string> 
 using namespace std;
+class string{
+public:
+
+private:
+    char* c_str;
+};
 int main()
 {
     string s1("hello");
     string s2(s1);
-    printf("%p\n",(s1.c_str));
+    printf("%p\n",s1);
     printf("%p\n",s2.c_str);
     return 0;
 
