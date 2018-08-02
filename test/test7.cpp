9e9e3f9a9ac697b9fcf4bffad56bc498e538139a
y6xiaomo <xiamo6512@163.com>
2018-08-02 10:28:23 +0800

    test7.cpp

---
+++ test/test7.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include<stack>
+using namespace std;
+class Solution
+{
+public:
+    void push(int node) {
+        int top;
+        while(1)
+        {
+           top = stack2.top();
+           stack1.push(top);
+           stack2.pop();
+
+        }
+        stack1.push(node);
+
+
+    }
+
+    int pop() {
+        int top;
+        while(1)
+        {
+            top = stack1.top();
+            stack2.push(top);
+            stack1.pop();
+
+        }
+        int pop;
+        if(!stack2,empty())
+        {
+            pop = stack2.top();
+            stack2.pop();
+            return pop;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+private:
+    stack<int> stack1;
+    stack<int> stack2;
+
+};
+int main()
+{
+    return 0;
+}
