093674bcb2a4f05b9b7babce222c860dd10e8d70
y6xiaomo <xiamo6512@163.com>
2018-08-03 10:13:50 +0800

    test10.cpp

--- test/test10.cpp
+++ test/test10.cpp
@@ -2,34 +2,56 @@
 class Solution
 {
 public:
-    //青蛙跳台阶问题
-    int jumpFloor(int number)
+    int jumpFloor(int n)
     {
-        if(number > 0)
-        { 
-
-            //有一个台阶，只能跳一次，一种跳法
-            //两级台阶，可以一次跳一个，也可以一次跳两个，两种跳法
-            if(number == 1 || number == 2)
-            {
-                return number;
-
-            }
-            //n = 1 --> 1
-            //n = 2 --> 2
-            //n = 3 --> 3
-            //n = 4 --> 5
-            //n = 5 --> 8
-            //n = 6 -->13
-            //f(n) = f(n-1)+f(n-2)
-            //第一级台阶的跳法决定了后面怎样跳
-            if(number > 2)
-            {
-                return jumpFloor(number-1)+jumpFloor(number-2);
-
-            }
+        if(n < 0)
+        {
+            return -1;
         }
-        return -1;
+        if(n == 1 || n == 2)
+        {
+            return n;
+        }
+        int result = 0;
+        int pre = 2;
+        int prepre = 1;
+        for(int i = 3;i <= n;i++)
+        {
+            result = pre + prepre;
+            prepre = pre;
+            pre = result;
+        }
+        return result;
+
     }
+    //青蛙跳台阶问题
+//    int jumpFloor(int number)
+//    {
+//        if(number > 0)
+//        { 
+//
+//            //有一个台阶，只能跳一次，一种跳法
+//            //两级台阶，可以一次跳一个，也可以一次跳两个，两种跳法
+//            if(number == 1 || number == 2)
+//            {
+//                return number;
+//
+//            }
+//            //n = 1 --> 1
+//            //n = 2 --> 2
+//            //n = 3 --> 3
+//            //n = 4 --> 5
+//            //n = 5 --> 8
+//            //n = 6 -->13
+//            //f(n) = f(n-1)+f(n-2)
+//            //第一级台阶的跳法决定了后面怎样跳
+//            if(number > 2)
+//            {
+//                return jumpFloor(number-1)+jumpFloor(number-2);
+//
+//            }
+//        }
+//        return -1;
+//    }
 
 };
