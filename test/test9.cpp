f7ad249b3ce39c2ae5dcfad763ee1013630f590e
y6xiaomo <xiamo6512@163.com>
2018-08-03 09:58:21 +0800

    test9.cpp

--- test/test9.cpp
+++ test/test9.cpp
@@ -2,33 +2,60 @@
 using namespace std;
 class Solution
 {
+    //下面用递归的解法，好多元素都重复了好几次
+    //时间复杂度以n的指数倍递增
+    //比如：我们要算第10次，必须知道第9次和第8次，知道第9次必须知道第8次和第7次
+    
 public:
     int Fibonacci(int n)
     {
-        //对程序时间有限制时，用下面这种判断，循环也一样
-        //不要写成if(n<=39 && n >= 0)
-        if(n > 39 && n < 0)
+        if(n < 0)
         {
             return -1;
         }
-        if(n == 0)
+        if(n == 0||n == 1)
         {
-            return 0;
-
+            return n;
         }
-        else if(n == 1|| n ==2)
+        int result = 0;
+        int pre = 1;
+        int prepre = 0;
+        for(int i = 2;i <= n;i++)
         {
-            return 1;
-
+            result = pre + prepre;
+            prepre = pre;
+            pre = result;
         }
-        else
-        {
-            return Fibonacci(n-1)+Fibonacci(n-2);
-            //第n次总是等于 n-1次 + n-1
-
-        }
-
-      }
+        return result;
+        
+    }
+//public:
+//    int Fibonacci(int n)
+//    {
+//        //对程序时间有限制时，用下面这种判断，循环也一样
+//        //不要写成if(n<=39 && n >= 0)
+//        if(n > 39 && n < 0)
+//        {
+//            return -1;
+//        }
+//        if(n == 0)
+//        {
+//            return 0;
+//
+//        }
+//        else if(n == 1|| n ==2)
+//        {
+//            return 1;
+//
+//        }
+//        else
+//        {
+//            return Fibonacci(n-1)+Fibonacci(n-2);
+//            //第n次总是等于 n-1次 + n-1
+//
+//        }
+//
+//      }
 };
 
 int main()
