da1e76ad91da3a392afd149036d12f40cebdab3a
y6xiaomo <xiamo6512@163.com>
2018-08-07 09:36:50 +0800

    test12.cpp

--- test/test12.cpp
+++ test/test12.cpp
@@ -1,89 +1,94 @@
 #include <iostream>
-class Solution {
+using namespace std;
+//求一个整数中1的个数
+class Solution
+{
 public:
-    bool dev_zeroError = false;
-    double Power(double base, int exponent) {
-        dev_zeroError = false;
-        double result = 1.0;
-
-        if(equal(base,0.0) && exponent < 0)
+    //最让面试官满意的升级版
+    //整数中有几个1就循环几次
+    int NumberOf1ExP(int n)
+    {
+        //节省时间，提高了效率
+        int count = 0;
+        while(n)
         {
-            //出错
-            //            dev_zeroError = true;
-            //                        result = 0.0;
-            //                                
+            count++;
+            //一个数减去1，相当与把它从右开始数的第一个1变成了0，
+            //把它后面的0全变成了1，它前面的保持不变
+            //比如1100，减一变成了 1011
+            //再将这个原本的整数与它减一后的数按位与，就把它最低位的1消掉了
+            // 1100 & 1011 = 1000 
+            // 所以这个整数有几个1就有几次这样的操作
+            n = n & (n-1);
         }
-        if(equal(base,0.0) && exponent >= 0)
-        {
-            return 0.0;
-
-        }
-        if(exponent == 0)
-        {
-            result = 1.0;
-
-        }
-        if(base > 0.0 && exponent > 0)
-        {
-            result = plusPower(base,exponent);
-
-        }
-        else if(base > 0.0 && exponent < 0)
-        {
-            exponent = -exponent;
-            result = plusPower(base,exponent);
-            result = 1.0/result;
-
-        }
-        if(base < 0)
-        {
-            base = -base;
-            if(exponent < 0)
-            {
-                exponent = -exponent;
-
-            }
-            result = plusPower(base,exponent);
-            result = -result;
-            if(exponent < 0)
-            {
-                result = 1.0 / result;
-
-            }
-
-        }
-        return result;
+        return count;
 
     }
-    double plusPower(double base,int exponent)
+    //为了避免负数出现死循环，将1左移依次和这个整数按位与
+    int NumberOf1Ex(int n)
     {
-        double result = 1.0;
-        if(base > 0 && exponent > 0)
+        int flag = 1;
+        int count = 0;
+        for(int i = 0;i < 32;i++)
         {
-            for(int i = 0;i < exponent;i++)
+            if(n & flag)
             {
-                result *= base;
+                count++;
+            }
+            flag = flag << 1;
+        }
+        return count;
+    }
+    //将这个数 & 1，然后每次右移一位，
+    //直到这个数变为0
+    int  NumberOf1(int n)
+    {
+        int count = 0;
+        for(int i = 0;i < 32;i++)
+        {
+            if(n & 1)
+            {
+                count++;
 
             }
+            n = n >> 1;
 
         }
-        return result;
-
-    }
-    int equal(double num1,int num2)
-    {
-
-        if((num1 - num2) > -0.00000001 && (num1 - num2) < 0.000000001)
-        {
-            return 1;
-
-        }
-        else 
-        {
-            return 0;
-
-        }
+        return count;
 
     }
 
 };
+
+int main()
+{
+    Solution num;
+    int ret = num.NumberOf1(-1);
+    cout<<"ret = "<<ret<<endl;
+
+    ret = num.NumberOf1(10);
+    cout<<"ret = "<<ret<<endl;
+
+    ret = num.NumberOf1(0);
+    cout<<"ret = "<<ret<<endl;
+
+    ret = num.NumberOf1Ex(-1);
+    cout<<"ret = "<<ret<<endl;
+
+    ret = num.NumberOf1Ex(10);
+    cout<<"ret = "<<ret<<endl;
+
+    ret = num.NumberOf1Ex(0);
+    cout<<"ret = "<<ret<<endl;
+
+    ret = num.NumberOf1ExP(-1);
+    cout<<"ret = "<<ret<<endl;
+
+    ret = num.NumberOf1ExP(10);
+    cout<<"ret = "<<ret<<endl;
+
+    ret = num.NumberOf1ExP(0);
+    cout<<"ret = "<<ret<<endl;
+
+
+}
