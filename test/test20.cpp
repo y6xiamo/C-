57362aa80011e3866813fac8c42de9d519e8e0be
y6xiaomo <xiamo6512@163.com>
2018-08-10 08:42:51 +0800

    test20.cpp

--- test/test20.cpp
+++ test/test20.cpp
@@ -2,14 +2,21 @@
 #include<stack>
 using namespace std;
 class Solution {
+    //实现一个最小栈，取最小元素的时间复杂度为O(1)
 public:
+    //定义两个栈，一个用来存数据，一个用来存最小元素
     void push(int value) {
+        //先将元素存入数据栈
         data_stack.push(value);
+
+        //如果最小栈为空，直接将该元素插入
         if(min_stack.empty())
         {
             min_stack.push(value);
 
         }
+        //如果最小栈的栈顶元素大于要插入的元素
+        //则把该元素入栈
         if(min_stack.top() > value)
         {
             min_stack.push(value);
