6d9f5649c76990b5b62300e7675ee54d44e7df5b
y6xiaomo <xiamo6512@163.com>
2018-08-15 17:09:22 +0800

    test29.cpp

--- test/test29.cpp
+++ test/test29.cpp
@@ -7,11 +7,13 @@ class Solution {
 public:
     vector<int> GetLeastNumbers_Solution(vector<int> input, int k) {
         vector<int> output;
+        //非法输入
         if(k > input.size())
         {
             return output;
-
         }
+        //先将这个数组排序
+        //然后输入前k个数
         sort(input.begin(),input.end());
         for(int i = 0;i < k;i++)
         {
