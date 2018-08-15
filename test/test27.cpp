64d78ab39d75bb3bd400b9003e81f0a5ba7a4557
y6xiaomo <xiamo6512@163.com>
2018-08-15 14:32:54 +0800

    test27.cpp

--- test/test27.cpp
+++ test/test27.cpp
@@ -4,22 +4,26 @@
 #include<vector>
 using namespace std;
 class Solution {
+    //打印出字符串中字符的所有排列组合
 public:
     vector<string> Permutation(string str) {
         vector<string> charArray;
 
         if(str.size() > 9 || str.size() <= 0)
         {
+            //字符长度为0或者大于9，不满足条件
             return charArray;
-
         }
 
+        //调用下面这个函数完成字符的所有组合
         PermutationHelp(charArray,str,0);
+        //将组合好的字符排列组合重新进行排序
         sort(charArray.begin(),charArray.end());
         return charArray; 
 
     }
 
+    //完成字符的所有组合
     void PermutationHelp(vector<string>& array,string str,int begin)
     {
         if(begin == str.size() - 1)
