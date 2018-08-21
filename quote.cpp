184095c3bf9320943c76fd2f46185857d4555590
y6xiaomo <xiamo6512@163.com>
2018-08-21 16:29:35 +0800

    quote.cpp

---
+++ quote.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+void Test()
+{
+   // int a = 1;
+   // int &b = a;
+   // int &c = b;
+   // cout<<"a:address->"<<&a<<endl;
+   // cout<<"b:address->"<<&b<<endl;
+   // cout<<"c:address->"<<&c<<endl;
+   
+    int d1 = 4;
+    const int& d2 = d1;
+//  //  d2 = 5;
+    d1 = 5;
+
+    const int a1 = 20;
+   // int & a2 = a1;
+   const int & a2 = a1;
+
+   double b1 = 1.2;
+//   int & b2 = b1;
+   const int & b2 = b1;   
+}
+int main()
+{
+    Test();
+    return 0;
+}
