6fcf2f9ce6257ec0b34e2f82c0ddbdf75201bd77
y6xiaomo <xiamo6512@163.com>
2018-08-21 20:47:40 +0800

    quote.cpp

--- quote.cpp
+++ quote.cpp
@@ -1,26 +1,42 @@
 #include <iostream>
 using namespace std;
+//void Test()
+//{
+//   // int a = 1;
+//   // int &b = a;
+//   // int &c = b;
+//   // cout<<"a:address->"<<&a<<endl;
+//   // cout<<"b:address->"<<&b<<endl;
+//   // cout<<"c:address->"<<&c<<endl;
+//   
+//    int d1 = 4;
+//    const int& d2 = d1;
+////  //  d2 = 5;
+//    d1 = 5;
+//
+//    const int a1 = 20;
+//   // int & a2 = a1;
+//   const int & a2 = a1;
+//
+//   double b1 = 1.2;
+////   int & b2 = b1;
+//   const int & b2 = b1;
+//}
+
+//值传递
+void Swap(int left,int right)
+{
+    int temp = left;
+    left = right;
+    right = temp;
+}
 void Test()
 {
-   // int a = 1;
-   // int &b = a;
-   // int &c = b;
-   // cout<<"a:address->"<<&a<<endl;
-   // cout<<"b:address->"<<&b<<endl;
-   // cout<<"c:address->"<<&c<<endl;
-   
-    int d1 = 4;
-    const int& d2 = d1;
-//  //  d2 = 5;
-    d1 = 5;
-
-    const int a1 = 20;
-   // int & a2 = a1;
-   const int & a2 = a1;
-
-   double b1 = 1.2;
-//   int & b2 = b1;
-   const int & b2 = b1;   
+    int a = 10;
+    int b = 20;
+    Swap(a,b);
+    cout<<"a = "<<a<<endl;
+    cout<<"b = "<<b<<endl;
 }
 int main()
 {
