05514fd50a14fd80babd88c35ed0c39e0f78fd18
y6xiaomo <xiamo6512@163.com>
2018-08-22 12:24:38 +0800

    vector.cpp

---
+++ vector.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+//1.迭代器
+//  正向
+//  反向
+//  const_xxx
+//
+//  1.for + operator[]
+//   作用：1.遍历 2.修改
+
+int main()
+{
+    Test();
+}
