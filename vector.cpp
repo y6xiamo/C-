ec8d13cf491ac4f4750c330bcaf8612df65786d5
y6xiaomo <xiamo6512@163.com>
2018-08-22 12:34:01 +0800

    vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -9,7 +9,29 @@ using namespace std;
 //
 //  1.for + operator[]
 //   作用：1.遍历 2.修改
+void print_vector(const vector<int> &v)
+{
+    //对vector而言最常用
+    for(size_t i = 0;i < v.size();i++)
+    {
+        cout<<v[i]<<" ";
+        cout<<endl;
+    }
 
+    //偶尔会用
+    vector<int>::iterator it = v.begin();
+    while(it != v.end())
+    {
+        cout<<*it<<" ";
+        it++;
+    }
+    cout<<endl;
+}
+
+void Test()
+{
+
+}
 int main()
 {
     Test();
