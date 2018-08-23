42997e7c9bb371e6cb61e71ed5d9e2372091b808
y6xiaomo <xiamo6512@163.com>
2018-08-23 23:48:14 +0800

    vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -12,52 +12,53 @@ using namespace std;
 void print_vector(const vector<int> &v)
 {
     //对vector而言最常用
-    for(size_t i = 0;i < v.size();i++)
-    {
-        cout<<v[i]<<" ";
-        cout<<endl;
-    }
+ //   for(size_t i = 0;i < v.size();i++)
+ //   {
+ //       cout<<v[i]<<" ";
+ //       cout<<endl;
+ //   }
 
-    //偶尔会用 迭代器
-//   vector<int>::iterator it = v.begin();
-//    while(it != v.end())
-//    {
-//        cout<<*it<<" ";
-//        it++;
-//    }
+ //   //偶尔会用 迭代器
+////   vector<int>::iterator it = v.begin();
+////    while(it != v.end())
+////    {
+////        cout<<*it<<" ";
+////        it++;
+////    }
 
-    //const迭代器
-    vector<int>::const_iterator it = v.begin();
-    while(it != v.end())
-    {
-       // *it = 10;
-        cout<<*it<<" ";
-        it++;
-    }
-    cout<<endl;
+ //   //const迭代器
+ //   vector<int>::const_iterator it = v.begin();
+ //   while(it != v.end())
+ //   {
+ //      // *it = 10;
+ //       cout<<*it<<" ";
+ //       it++;
+ //   }
+ //   cout<<endl;
 
-    for(int j = v.size() - 1;j >= 0;j--)
-    {
-        cout<<v[j]<<" ";
-    }
+ //   for(int j = v.size() - 1;j >= 0;j--)
+ //   {
+ //       cout<<v[j]<<" ";
+ //   }
+ //   cout<<endl;
 
 
     //反向迭代器
-  //  vector<int> reverse_iterator rit = v.rbegin();
-  //  while(rit != v.rend())
-  //  {
-  //      *rit = 10;
-  //      cout<<*rit<<" ";
-  //      rit++;
-  //  }
+    vector<int>::reverse_iterator rit = v.rbegin();
+    while(rit != v.rend())
+    {
+        cout<<*rit<<" ";
+        rit++;
+    }
+    cout<<endl;
 
-  //  vector<int>::rever_iterator rit = v.begin();
+ //   vector<int>::const_rever_iterator it = v.begin();
 
-  //  while(rit != v.end())
-  //  {
-  //      cout<<*rit<<" ";
-  //      ++rit;
-  //  }
+ //   while(rit != v.end())
+ //   {
+ //       cout<<*rit<<" ";
+ //       ++rit;
+ //   }
 }
 
 void Test()
