e8d31f0fc2f4d126d9031fbce3a15172daa586dd
y6xiaomo <xiamo6512@163.com>
2018-08-24 23:30:31 +0800

    test47.cpp

--- test/test47.cpp
+++ test/test47.cpp
@@ -3,7 +3,9 @@ class Solution {
 public:
     //求1+2+3+...+n，要求不能使用乘除法、for、while、if、else、
     //switch、case等关键字及条件判断语句（A?B:C）
-    //1.调用递归，
+    //1.调用递归，利用&&来规定递归的终止条件
+    //n <= 0 ---> 直接返回该数字
+    //n > 0 ---> 执行递归
     int Sum_Solution(int n) {
         int result = n;
         bool ret = (n > 0)&&((result += Sum_Solution(n-1)) > 0);
