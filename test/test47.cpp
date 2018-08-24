f13383b09b304ba48e2a215377f4c57e45575a81
y6xiaomo <xiamo6512@163.com>
2018-08-24 23:28:37 +0800

    test47.cpp

--- test/test47.cpp
+++ test/test47.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 class Solution {
 public:
+    //求1+2+3+...+n，要求不能使用乘除法、for、while、if、else、
+    //switch、case等关键字及条件判断语句（A?B:C）
+    //1.调用递归，
     int Sum_Solution(int n) {
         int result = n;
         bool ret = (n > 0)&&((result += Sum_Solution(n-1)) > 0);
