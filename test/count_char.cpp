96c227006e6bd41374dee756c77e30c9c89aa4d8
y6xiaomo <xiamo6512@163.com>
2018-08-25 11:23:59 +0800

    count_char.cpp

---
+++ test/count_char.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int GetNumberOfK(vector<int> data ,int k) {
+        if(data.size() == 0)
+        {
+            return 0;
+
+        }
+        int count = 0;
+        for(size_t i = 0;i < data.size();i++)
+        {
+            if(data[i] == k)
+            {
+                count++;
+
+            }
+
+        }
+        return count;
+
+    }
+
+};
